0beeda06bcdbdcca9e6eca0222275113e7dbe23b
pdavid88 <david.alg88@gmail.com>
2024-04-30 12:45:59 +0200

    ControlClientes 6.0 - Seguridad

--- src/main/java/com/david/web/ControladorInicio.java
+++ src/main/java/com/david/web/ControladorInicio.java
@@ -5,6 +5,8 @@ import com.david.servicio.IPersonaService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -20,8 +22,9 @@ public class ControladorInicio {
     private IPersonaService personaService;
 
     @GetMapping("/")
-    public String inicio(Model model) {
+    public String inicio(Model model, @AuthenticationPrincipal User user) {
         var personas = personaService.listarPersonas();
+        log.info("Usuario que hizo login: " + user);
         model.addAttribute("personas", personas);
         return "index";
     }
--- src/main/java/com/david/web/WebConfig.java
+++ src/main/java/com/david/web/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -30,6 +31,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registro) {
         registro.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registro){
+        registro.addViewController("/").setViewName("index");
+        registro.addViewController("/login");
+        registro.addViewController("/errors/403").setViewName("/errors/403");
     }
 
 }
